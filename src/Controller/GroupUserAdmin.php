6ad77a2a2bcf6a7091030baa424a26eb0a84ef8a
neq <neq@o2.pl>
2025-02-05 11:41:37 +0100

    date

--- src/Controller/GroupUserAdmin.php
+++ src/Controller/GroupUserAdmin.php
@@ -51,6 +51,85 @@ class GroupUserAdmin extends Controller
 		$this->paginator = $paginator;
 	}
 
+	public  function getMinutes($minut)
+	{
+// j.pol
+	 switch($minut)
+	 {
+			case 0: return 0; break;
+			case 1: return 1; break;
+			case ($minut >= 2 && $minut <= 4):
+			case ($minut >= 22 && $minut <= 24):
+			case ($minut >= 32 && $minut <= 34):
+			case ($minut >= 42 && $minut <= 44):
+			case ($minut >= 52 && $minut <= 54): return "$minut minuty temu"; break;
+			default: return "$minut minut temu"; break;
+	}
+	return -1;
+
+	}
+public function formatujDate($data_wejsciowa){
+
+$timestamp = strtotime($data_wejsciowa);
+
+$now = time();
+
+if ($timestamp > $now) {
+  return 'Podana data nie może być większa od obecnej.';
+}
+
+$diff = $now - $timestamp;
+
+$minut = floor($diff/60);
+$godzin = floor($minut/60);
+$dni = floor($godzin/24);
+
+if ($minut <= 60) {
+  $res = $this->getMinutes($minut);
+  switch($res)
+  { 
+	case 0: return "przed chwilą";
+	case 1: return "minutę temu";
+	default: return $res;
+  }
+}
+
+$timestamp_wczoraj = $now-(60*60*24);
+$timestamp_przedwczoraj = $now-(60*60*24*2);
+
+if ($godzin > 0 && $godzin <= 6) {
+
+  $restMinutes = ($minut-(60*$godzin));
+  $res = $this->getMinutes($restMinutes);
+  if ($godzin == 1) {
+	return "Godzinę temu ";//.$res
+  } else {
+  if ($godzin >1 && $godzin<5)return "$godzin godziny temu ";
+  if ($godzin >4)return "$godzin godzin temu";
+  }
+
+} else if (date("Y-m-d", $timestamp) == date("Y-m-d", $now)) {//jesli dzisiaj
+  return "Dzisiaj ".date("H:i", $timestamp);
+} else if (date("Y-m-d", $timestamp_wczoraj) == date("Y-m-d", $timestamp)) {//jesli wczoraj
+  return "Wczoraj ".date("H:i", $timestamp);
+} else if (date("Y-m-d", $timestamp_przedwczoraj) == date("Y-m-d", $timestamp)) {//jesli wczoraj
+  return "Przedwczoraj ".date("H:i", $timestamp);
+}
+
+switch($dni)
+{ 
+  case ($dni < 7): return "$dni dni temu, ".date("Y-m-d", $timestamp); break;
+  case 7: return "Tydzień temu, ".date("Y-m-d", $timestamp); break;
+  case ($dni > 7 && $dni < 14): return "Ponad tydzień temu, ".date("Y-m-d", $timestamp); break;
+  case 14: return "Dwa tygodznie temu, ".date("Y-m-d", $timestamp); break;
+  case ($dni > 14 && $dni < 30): return "Ponad 2 tygodnie temu, ".date("Y-m-d", $timestamp); break;
+  case 30: case 31: return "Miesiąc temu"; break;
+  case ($dni > 31): return date("Y-m-d H:i", $timestamp); break;
+}
+return date("Y-m-d", $timestamp);
+}
+
+
 	public function index($page, PaginatorInterface $paginator)
 	{
 
@@ -148,9 +227,28 @@ class GroupUserAdmin extends Controller
 			$lastcr = $query->getArrayResult();
 
 			$last["$gid"] = $lastcr[0][1];
+			$lastf["$gid"] = $this->formatujDate($lastcr[0][1]);
+
+						#NEQ
+						$query2 = $em->createQuery('select p.created_at from App\Entity\Group p where p.id = :gid')->setParameter('gid', $gid);
+						$lastcr2 = $query2->getArrayResult();
+			
+						#echo "<pre>"; var_dump($lastcr2[0]["created_at"]); echo "</pre>";
+			
+						$toiter = $lastcr2[0]["created_at"];
+			
+						while(list($k, $v) = each($toiter))
+						{
+							if($k == "date")
+							{
+								$luty["$gid"] = $this->formatujDate($v);
+							}
+						}
+		
+		
 		}
 
-		return $this->render('port119/admgroup.html.twig', [ 'navi' => $navi, 'thread_admin' => $thread_admin, 'username' => $username, 'abouts' => $abouts, 'gcdn' => $gcdn, 'admin' => $admin, 'ccnt' => $ccnt,'last' => $last]);
+		return $this->render('port119/admgroup.html.twig', [ 'luty' => $luty, 'lastf' => $lastf, 'navi' => $navi, 'thread_admin' => $thread_admin, 'username' => $username, 'abouts' => $abouts, 'gcdn' => $gcdn, 'admin' => $admin, 'ccnt' => $ccnt,'last' => $last]);
 	}
 }
 
